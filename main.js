27965278ac591cb9ce8cf99591a753ea83a2c14f
johanSarmiento <johansarmiento@gmail.com>
2023-02-24 07:24:01 -0500

    Listo ahora si

--- main.js
+++ main.js
@@ -52,9 +52,10 @@ function aniadir(){
     dia.value=''
     
     let contenido=fecha1.join("/") //Guardamos la fecha en una variable y la separamos por guion
+    
     contenido+=`    Esto es un stinrg`
     document.getElementById('pantalla2').innerHTML=contenido //imprimimos en html
-    
+
 }
 
 
@@ -62,7 +63,9 @@ function fParse(){
     //No es necesario hacer el cambio a numeros
     
     let fecha3=new Date(Date.parse(fecha1)) // usamos el metodo
-    
+    // Esto crea un nuevo objeto con la fecha en formato DD-MM-YY
+    //Se usa con strings
+
     contenido=fecha3
     document.getElementById('pantalla').innerHTML=contenido
     contenido=''
@@ -72,20 +75,21 @@ function fParse(){
 
 function fNumber(){
 
+    // let fecha=[2022,1,5]
+    // let fecha2=new Date(Date.UTC(...fecha))  //Metodo de propragacion
+    // Fri Feb 04 2022 19:00:00 GMT-0500 (hora estándar de Colombia)
+    
+    //explicar
+
     let fechaNumber=fecha1.map(elemento => parseInt(elemento)) //Pasamos los valores a numeros
     fechaNumber[1]=fechaNumber[1]-1 
     fechaNumber[2]=fechaNumber[2]+1
     // Se cambia el valor del mes y del dia ya que en el sistema empieza en 0
     
-    let fecha2=new Date(Date.UTC(...fechaNumber)) //Usamos operador de propagacion para tomar cada elemento del array
-    
+    let fecha2=new Date(Date.UTC(...fechaNumber)) 
+    //Usamos operador de propagacion para tomar cada elemento del array
     //el Metodo UTC recorre desde 0
     
-    // let fechaNumber=[2022,1,5]
-    // let fecha2=new Date(Date.UTC(...fecha))
-    // Fri Feb 04 2022 19:00:00 GMT-0500 (hora estándar de Colombia)
-    
-    //explicar
     
     contenido=fecha2
     document.getElementById('pantalla').innerHTML=contenido
@@ -126,8 +130,43 @@ function tMes(){  //Trae el mes
 function tDia(){ //Trae el dia
 
     date2=date1.getDate();
-    
+
     contenido=date2
     document.getElementById('pantalla').innerHTML=date2
     contenido=''
+}
+
+function sets(){
+    let fecha6=new Date()
+    fecha6.setFullYear(2021);
+    fecha6.setHours(13);
+    fecha6.setMinutes(30);
+    console.log(fecha6)
+}
+
+//sets()
+
+
+function zonas(){
+
+    //Zonas horarias
+    // Obtener fecha y hora en la zona horaria local del usuario
+    let fechaLocal=new Date()
+    fechaLocal = fechaLocal.toLocaleString();
+    console.log(fechaLocal)
+
+    // Utilizar bibliotecas de manejo de zonas horarias, como moment-timezone o luxon
+}
+
+//zonas()
+
+function operaciones(){
+
+    let fechaOp=new Date(Date.UTC(2021,8,5))
+    let fechaOp2=new Date(Date.UTC(2023,8,5))
+
+    //comparar fechas
+
+    
+
 }
